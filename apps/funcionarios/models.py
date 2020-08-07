5c4908dd7a61f67c29ec33eaa7e04fd195ebeff5
kfbueno <kfbueno@globo.com>
2020-08-07 20:15:05 -0300

    criando relacionamento de banco de dados

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamentos   # um usuario pode estar em multiplos departamentos
+from apps.empresas.models import Empresa
 
 
 # Create your models here.
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)    # Protect nao apaga em cascata
+    departamentos = models.ManyToManyField(Departamentos)   # foreingkey de um para n
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+
 
     def __str__(self):
         return self.nome
