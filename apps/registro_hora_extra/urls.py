7fd9440a22a2dc9e26ab466ffe365c08c3619467
kfbueno <kfbueno@globo.com>
2020-08-14 16:01:37 -0300

    listagem de hora extra

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import HoraExtraList
+from .views import HoraExtraList, HoraExtraEdit
 
 ''', HoraExtraEdit, HoraExtraDelete, HoraExtraNovo'''
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
+    path('editar/<int:pk>', HoraExtraEdit.as_view(), name='update_hora_extra'),
 ]
 
-#  path('editar/<int:pk>', HoraExtraEdit.as_view(), name='update_hora_extra'),
+
 #  path('deletar/<int:pk>', HoraExtraDelete.as_view(), name='delete_hora_extra'),
 #  path('novo/', HoraExtraNovo.as_view(), name='create_hora_extra'),
 
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 from .models import RegistroHoraExtra
 
 
@@ -9,4 +9,9 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada) #envia para o queryset o filtro desejado
+
+class HoraExtraEdit(UpdateView):
+        model = RegistroHoraExtra
+        fields = ['motivo', 'funcionario', 'horas']
+
 
