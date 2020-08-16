87a4e70a773228bf7a478cbb8c4aeca445168f15
kfbueno <kfbueno@globo.com>
2020-08-16 13:14:50 -0300

    correcao settings 4

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -126,13 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "/static/"),
 ]
 
-STATIC_FILES_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_FILES_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'home'
 
