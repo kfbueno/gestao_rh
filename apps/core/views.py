60686b2e2c186b96c6781fc42cf62aacc37ce384
kfbueno <kfbueno@globo.com>
2020-08-25 14:50:37 -0300

    expondo api rest framework

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from apps.funcionarios.models import Funcionario    #importo para identificar o funcionario que vai logar
 
+# importações do restframework
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from apps.core.serializers import UserSerializer, GroupSerializer
+
 
 # Create your views here.
 @login_required #impede que acesse o sistema sem estar logado
@@ -9,3 +14,11 @@ def home(request):
     data = {}
     data['usuario'] = request.user
     return render(request, 'core/index.html', data)
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+
+# imports do rest framework
+from rest_framework import routers
+from apps.core import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include('apps.core.urls')),
@@ -28,6 +37,9 @@ urlpatterns = [
     path('horas-extras/', include('apps.registro_hora_extra.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 '''
